64833db9287685b6c3daf54af9305abe27deb7cf
Mike Orozco <mike.orozco94@gmail.com>
2015-08-24 22:18:47 +0000

    ITMK_WP_ADVERT_CONTENT.php edited online with Bitbucket

--- ITMK_WP_ADVERT_CONTENT.php
+++ ITMK_WP_ADVERT_CONTENT.php
@@ -1,1 +1,18 @@
-<p>Put content that you want displayed here.  This will show for anyone who has the plugin installed.</p>
+<?php 
+    /*
+        You can show different content depending on requester IP.
+        Can modify this to go by URL if needed.  Requires edit to the initial script.
+    */
+	
+	/* Base new output functions off of how the outputDefault function below is formatted */
+    
+    if($_SERVER['REMOTE_ADDR'] == "108.167.189.47") {
+        outputDefault();
+    } else {
+        outputDefault();
+    }
+?>
+
+<?php function outputDefault() { ?>
+    <p>Put HTML content that you want displayed here.  This will show for anyone who has the plugin installed.</p>
+<?php } ?>
